1d44149fcce6c8111523433e5c01d45bac9440c4
juancho87-bit <juan2532david@gmail.com>
2025-09-01 23:23:10 -0500

    comentando el codigo

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 // empezando a trabajar el desafio challenge del amigo secreto
 
-let amigos = [];
+let amigos = []; // donde vamos a guardar los nombres que el usuario escriba
 
-function agregarAmigo (){
+function agregarAmigo (){  //la funcion cuando damos en onclik en html del boton añadir
 
     let input = document.querySelector ("#amigo");
     let nombre = input.value.trim();
 
         if (nombre !==""){
-            amigos.push(nombre); // lo guarda en el vector 
-            mostrarAmigos();     
-            input.value =" ";
+            amigos.push(nombre); // con push agrega o lo guarda en el vector 
+            mostrarAmigos();     // llama a la funcion que esta abajo y muestra los amigos que hemos agregado
+            input.value =" "; // limpia el cuadro de texto para seguir escribiendo
         }
 }
 
